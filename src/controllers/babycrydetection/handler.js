88f166b1579cd5ba82d9631b640b56b3935fc418
albimdkr <albimudakar04@gmail.com>
2023-12-09 22:47:26 +0700

    feat(API)! : adding handler babycrydetection

--- src/controllers/babycrydetection/handler.js
+++ src/controllers/babycrydetection/handler.js
@@ -1,86 +1,45 @@
-/* eslint-disable no-undef */
 /* eslint-disable import/no-extraneous-dependencies */
-const fs = require('fs');
-const tf = require('@tensorflow/tfjs-node');
-const librosa = require('librosa');
-const wav = require('node-wav');
-const noisegate = require('noise-gate');
+const axios = require('axios');
+const recommendationsService = require('../../services/recommendations');
 
-async function loadModel() {
-  const modelPath = 'https://storage.googleapis.com/disada-model/model_saved_model.json';
-  loadedModel = await tf.loadLayersModel(`file://${modelPath}`);
-}
+const handleFlaskResult = async (audioFilePath) => {
+  try {
+    // handler process from flask
+    const flaskResponse = await axios.post('flask', {
+      audio: audioFilePath,
+    });
+    const { predictedLabel } = flaskResponse.data;
 
-async function extractFeaturesNewAudio(filePath) {
-  const audioBuffer = fs.readFileSync(filePath);
+    // get recommend by predict
+    const recommendations = recommendationsService.getRecommendation(predictedLabel);
 
-  const decodedAudio = wav.decode(audioBuffer);
+    //  send respons JSON
+    return { recommendations };
+  } catch (error) {
+    console.error('Error in handleFlaskResult:', error);
+    throw new Error('Gagal dalam menangani hasil ekstraksi dari Flask');
+  }
+};
 
-  // Extract audio features
-  let y = decodedAudio.channelData[0]; 
-  const sr = decodedAudio.sampleRate;
+const handleNodeJsEndpoint = async (flaskResult) => {
+  try {
+    // get predict from flask
+    const { predictedLabel } = flaskResult;
 
-  // Apply noise gate to reduce noise
-  y = noisegate(y, { threshold: 0.1, attack: 10, release: 200 });
+    // get recommend by predict
+    const recommendations = recommendationsService.getRecommendation(predictedLabel);
 
-  // Placeholder logic
-  const mfccs = librosa.feature.mfcc(y, sr, { n_mfcc: 13 });
-  const spectralCentroid = librosa.feature.spectral_centroid(y, sr)[0];
-  const spectralBandwidth = librosa.feature.spectral_bandwidth(y, sr)[0];
-  const spectralContrast = librosa.feature.spectral_contrast(y, sr);
-  const chroma = librosa.feature.chroma_stft(y, sr);
-  const zeroCrossings = librosa.feature.zero_crossing_rate(y);
-  const rmse = librosa.feature.rms(y);
-  const onsetEnv = librosa.onset.onset_strength(y, sr);
-  const tempo = librosa.beat.tempo({ onset_envelope: onsetEnv, sr });
-
-  // Calculate means along the axis (axis=1 for spectral_contrast and chroma)
-  const meanMfccs = tf.mean(mfccs, 1).arraySync();
-  const meanSpectralCentroid = tf
-    .mean(tf.tensor(spectralCentroid))
-    .arraySync()[0];
-  const meanSpectralBandwidth = tf
-    .mean(tf.tensor(spectralBandwidth))
-    .arraySync()[0];
-  const meanSpectralContrast = tf.mean(spectralContrast, 1).arraySync();
-  const meanChroma = tf.mean(chroma, 1).arraySync();
-  const meanZeroCrossings = tf.mean(tf.tensor(zeroCrossings)).arraySync()[0];
-  const meanRmse = tf.mean(tf.tensor(rmse)).arraySync()[0];
-
-  // Concatenate features into a single vector
-  const featureVector = [
-    ...meanMfccs,
-    meanSpectralCentroid,
-    meanSpectralBandwidth,
-    ...meanSpectralContrast,
-    ...meanChroma,
-    meanZeroCrossings,
-    meanRmse,
-    tempo,
-  ];
-
-  // Reshape feature vector to match model input shape
-  const reshapedFeatureVector = tf.tensor2d([featureVector]);
-
-  return reshapedFeatureVector.arraySync()[0];
-}
-
-async function predictLabel(audioFeatures) {
-  const inputTensor = tf.tensor2d([audioFeatures]);
-
-  // Perform prediction
-  const prediction = loadedModel.predict(inputTensor);
-  const predictionData = await prediction.data();
-
-  // Get predicted class and label
-  const predictedClass = tf.argMax(predictionData).dataSync()[0];
-  const predictedLabel = labelMapping[predictedClass];
-
-  return { predictedLabel, predictionProbabilities: predictionData };
-}
+    // send response JSON
+    return { recommendations };
+  } catch (error) {
+    console.error('Error in handleNodeJsEndpoint:', error);
+    throw new Error(
+      'Gagal dalam menangani hasil ekstraksi di endpoint Node.js lainnya',
+    );
+  }
+};
 
 module.exports = {
-  loadModel,
-  extractFeaturesNewAudio,
-  predictLabel,
+  handleFlaskResult,
+  handleNodeJsEndpoint,
 };
