ab89e8fb8d6583fe314f6c455361833f1fd2eda3
albimdkr <albimudakar04@gmail.com>
2023-12-16 14:40:22 +0700

    fix: change env

--- scripts/test-db-connection.js
+++ scripts/test-db-connection.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 const config = require('../src/config/database');
 
-const sequelize = new Sequelize(config.development);
+const sequelize = new Sequelize(config.production);
 
 async function testDatabaseConnection() {
   try {
